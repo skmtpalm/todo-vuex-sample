5a0bbcd27700458ef0be78d26ce39b7dad31d216
skmtpalm <hachihachi.8og@gmail.com>
2017-11-06 18:23:40 +0900

    Add Static tasks

---
+++ src/compoments/Task.vue
@@ -0,0 +1,47 @@
+<template>
+  <li class="task__item">
+    <div class="level">
+      <div class="level-left">
+        <div class="level-item">
+          <input type="checkbox">
+        </div>
+        <div class="level-item task__item__title">
+          {{ task.title }}
+        </div>
+      </div>
+
+      <div class="level-right">
+        <div class="level-item task__item__due">
+          <time>{{ task.dueDate }}</time>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['task']
+}
+</script>
+
+<style lang="scss" scoped>
+$right-gray: hsl(0, 0%, 86%);
+$gray: hsl(0, 0%, 50%);
+  .task__item {
+    padding: 0.5rem 0;
+    border-top: 1px solid $right-gray;
+    &:last-child {
+      border-bottom: 1px solid $right-gray;
+    }
+    &__title {
+    }
+    &__due {
+      font-size: 0.8em;
+      color: $gray;
+    }
+  }
+  input[type='checkbox'] {
+    margin-right: 6px;
+  }
+</style>
--- src/compoments/Tasks.vue
+++ src/compoments/Tasks.vue
@@ -1,13 +1,48 @@
 <template>
-  <main>
-    <p>Tasks</p>
+  <main class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-8 is-narrow">
+          <h3 class="subtitle">All Tasks</h3>
+          <ul>
+            <task
+                v-for="task in tasks"
+                :task="task"
+                :key="task.id"
+                ></task>
+            <li class="task__add">+ New Task</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+import Task from './Task.vue'
+
 export default {
+  data() {
+  },
+  components: {
+    Task
+  },
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: 'Hello1', dueDate:'11月6日', isDone: false },
+        { id: 2, title: 'Hello2', dueDate:'11月7日', isDone: false },
+        { id: 3, title: 'Hello3', dueDate:'11月8日', isDone: false },
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="css">
+.task__add {
+  color: red;
+  margin-top: 8px;
+  cursor: pointer;
+}
 </style>
